f6fc44233f168e92437e58c7e0e3d30184bf945e
Ieuan <ieuan@onda.me>
2019-04-19 16:13:54 +0900

    sdk init

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,15 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Component, OnInit } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Component, OnInit } from "@angular/core";
+const irsdk: any = require("node-irsdk");
 @Component({
-  selector: 'App',
+  selector: "App",
   templateUrl: "views/components/app/index.html"
 })
 export class AppComponent implements OnInit {
-  public readonly name = 'electron-forge';
+  public readonly name = "electron-forge";
 
-  ngOnInit(): void {
-    console.log('component initialized');
+  public ngOnInit(): void {
+    console.log("component initialized");
+
+    irsdk.init({
+      telemetryUpdateInterval: 64, // 15 ticks per second
+      sessionInfoUpdateInterval: 2000
+    });
+
+    const iracing: any = irsdk.getInstance();
+
+    console.log("waiting for iRacing...");
+
+    iracing.on("Connected", function (): void {
+      console.log("connected to iRacing..");
+    });
+
+    iracing.on("Disconnected", function (): void {
+      console.log("iRacing shut down, exiting.\n");
+      // process.exit();
+    });
+
+    iracing.on("TelemetryDescription", function (data: any): void {
+      console.log("got TelemetryDescription");
+    });
+
+    iracing.on("Telemetry", function (data: any): void {
+    });
+
+    iracing.on("SessionInfo", function (data: any): void {
+    });
   }
 }
 
