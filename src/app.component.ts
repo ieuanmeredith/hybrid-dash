e34c50d054bbe5f259cb000970888cd89b794284
Ieuan Meredith <ieuanmeredith@googlemail.com>
2019-04-21 23:11:29 +0900

    prototyping

--- src/app.component.ts
+++ src/app.component.ts
@@ -8,10 +8,18 @@ const irsdk: any = require("node-irsdk");
 })
 export class AppComponent implements OnInit {
   public readonly name = "electron-forge";
+  public soc: number = 0;
+  public deploy: number = 0;
+  public flags: [];
+  public deltaToSesBest: number;
+  public lapsLeft: number;
+  public timeLeft: number;
 // EnergyERSBatteryPct
 // EnergyMGU_KLapDeployPct
   public ngOnInit(): void {
     console.log("component initialized");
+
+    const that = this;
 
     irsdk.init({
       telemetryUpdateInterval: 16, // 60 ticks per second
@@ -36,6 +44,12 @@ export class AppComponent implements OnInit {
     });
 
     iracing.on("Telemetry", function (data: any): void {
+      that.soc = Math.floor(data.values.EnergyERSBatteryPct *  100);
+      that.deploy = Math.floor(data.values.EnergyMGU_KLapDeployPct * 100);
+      that.flags = data.values.SessionFlags;
+      that.deltaToSesBest = data.values.LapDeltaToSessionBestLap.toFixed(2);
+      that.lapsLeft = data.values.SessionLapsRemainEx.toFixed(2);
+      that.timeLeft = data.values.SessionTimeRemain.toFixed(2);
     });
 
     iracing.on("SessionInfo", function (data: any): void {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from 'electron';
-import { enableLiveReload } from 'electron-compile';
+import { app, BrowserWindow } from "electron";
+import { enableLiveReload } from "electron-compile";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,13 +7,14 @@ let mainWindow: Electron.BrowserWindow | null;
 
 const isDevMode = process.execPath.match(/[\\/]electron/);
 
-if (isDevMode) enableLiveReload();
+if (isDevMode) { enableLiveReload(); }
 
 const createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    alwaysOnTop: true
   });
 
   // and load the index.html of the app.
@@ -25,7 +26,7 @@ const createWindow = async () => {
   }
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -36,18 +37,18 @@ const createWindow = async () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
